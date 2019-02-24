b11484891b79ed33dc625a2b8f3f40eee1bcae63
Roy <roy@webiks.com>
2019-02-24 17:35:12 +0200

    transparency is working.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
+
+  transparentBaseLayer = new Cesium.SingleTileImageryProvider({
+    url : 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNgYAAAAAMAASsJTYQAAAAASUVORK5CYII='
+  });
+
+  viewer = new Cesium.Viewer('cesiumContainer', {
+    skyBox : false,
+    skyAtmosphere : false,
+    baseLayerPicker : false,
+    imageryProvider : this.transparentBaseLayer,
+    contextOptions : {
+      webgl: {
+        alpha: true
+      }
+    }
+  });
+
 
   ngOnInit() {
+    const viewer = new Cesium.Viewer(this.el.nativeElement);
+    this.initMap();
+  }
+
+  initMap() {
+    this.viewer.scene.backgroundColor = Cesium.Color.TRANSPARENT;
+    this.viewer.scene.globe.baseColor = Cesium.Color.TRANSPARENT;
+
+
   }
 
 }
